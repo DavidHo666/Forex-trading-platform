27dedfb232e024295f79019d8be3c22d3a8094ea
Dawei He <89663639+DavidHo666@users.noreply.github.com>
2023-05-08 12:29:31 -0400

    Update FetchMultipleForexRate.py

--- back_end/FetchMultipleForexRate.py
+++ back_end/FetchMultipleForexRate.py
@@ -1,8 +1,11 @@
 import requests
 import json
 import boto3
+from datetime import datetime, date
 from botocore.exceptions import ClientError
 
+
+api_key = 'fa8e9199c2-b3d35b2bf0-rsignh'
 
 
 def insert_data(data, db=None, table='CurrencyRate'):
@@ -12,6 +15,45 @@ def insert_data(data, db=None, table='CurrencyRate'):
     response = table.put_item(Item=data)
     print('@insert_data: response', response)
     return response
+
+def rolling_update(data, db=None, table='LatestRates'):
+    if not db:
+        db = boto3.resource('dynamodb')
+    table = db.Table(table)
+    for currency in data['results']:
+        print("rolling update:", currency)
+        currency_record = table.get_item(Key={'currency': currency})['Item']['rates']
+        if datetime.strptime(data['update_time'][:10], "%Y-%m-%d") > datetime.strptime(currency_record['update_time'][:10], "%Y-%m-%d"):
+            currency_record['highest'] = data['results'][currency]
+            currency_record['lowest'] = data['results'][currency]
+        currency_record['latest_rate'] = data['results'][currency]
+        currency_record['highest'] = max(data['results'][currency], currency_record['highest'])
+        currency_record['lowest'] = min(data['results'][currency], currency_record['lowest'])
+        currency_record['update_time'] = data['update_time']
+        res = update_rate({'currency': currency}, currency_record)
+        print("rolling update:",res)
+        
+
+def update_rate(key, feature, db=None, table='LatestRates'):
+    if not db:
+        db = boto3.resource('dynamodb')
+    table = db.Table(table)
+    # change user balance
+    response = table.update_item(
+        Key=key,
+        UpdateExpression="set #feature=:f",
+        ExpressionAttributeValues={
+            ':f': feature
+        },
+        ExpressionAttributeNames={
+            "#feature": "rates"
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+    print(response)
+    return response
+
+
 
 
 def lambda_handler(event, context):
@@ -30,8 +72,9 @@ def lambda_handler(event, context):
     response.pop('updated', None)
     for k in response['results']:
         response['results'][k] = str(response['results'][k])
-
+    print("modified_response: ", response)
     insert_data(response)
+    rolling_update(response)
 
     return {
         'statusCode': 200,
